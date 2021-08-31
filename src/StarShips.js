3e6977f7b44cf01826bc25d62fbf5688147142f9
marsalxavi <marsalxavi@gmail.com>
2021-08-31 18:19:06 +0200

    S8N01Ex03

--- src/StarShips.js
+++ src/StarShips.js
@@ -1,50 +1,45 @@
 
 import React, { Component, useEffect, useState } from 'react'
 
-// Crida a la API
-async function GetData() {
-    const shipsFetch = await fetch('https://swapi.dev/api/starships/');
+// Modificat al Nivell 1 Exercici 3
+// Crida a la API afegint Num Pag
+async function GetPage(PageNum) {
+    const urlPage = new URL(`https://swapi.dev/api/starships/?page=${PageNum}`)
+    const shipsFetch = await fetch(urlPage);
     const shipsJson = await shipsFetch.json();
     const shipsArray = shipsJson.results
     return (shipsArray);
 }
 
-// Retorna llistat de naus: nom i model
+// ============ Retorna llistat de naus: nom i model ============
 export function StarShips() {
     // Definim l' estat de les naus
-    const [totesNaus, setTotesNaus] = useState([])
-    const [unaNau, setUnaNau] = useState([])
-
-    // Obtenim el llistat de Naus i l' assignem a l' estat (componentDidMount)
-    useEffect(() => {
-        (async () => {
-            const data = await GetData()
-            setTotesNaus(data)
-        }
-        )
-            ()
-    }, []);
-
-    //Definim estil de linia
+    const [PaginaNaus, setPaginaNaus] = useState([]) // Canvi Tots x Pag al N01Ex03
+    //Definim estil de linia (Afegit al Nivell 1 Exercici 2
     var liniaEsquerra = { textAlign: 'Left' }
 
     // Totes les naus
     // Retorna nom - model
     const NomImodel = (aryUnaNau) => ([aryUnaNau.name, aryUnaNau.model].join(" / "))
 
-    // Obte una nau per l index
-    const GetUnaNau = (aryUnaNau) => setUnaNau(aryUnaNau)
-
     // Llista totes les naus per pantalla
-    function PrintTotesNaus() {
+    function PrintPaginaNaus() {
         return (
-            totesNaus.map((aryUnaNau, index) =>
+            PaginaNaus.map((aryUnaNau, index) =>
             (<div key={index} style={liniaEsquerra}>
+                {/* Modificat al N01Ex02 */}
                 <li onClick={() => GetUnaNau(aryUnaNau)} > {NomImodel(aryUnaNau)}</li>
             </div>)
             )
         )
     }
+
+
+    // ========= Afegit al Nivell 1 Exercici 2 =========
+    const [unaNau, setUnaNau] = useState([])
+
+    // Obte una nau per l index
+    const GetUnaNau = (aryUnaNau) => setUnaNau(aryUnaNau)
 
     // Llista una Nau per pantalla
     function FitxaUnaNau() {
@@ -59,6 +54,24 @@ export function StarShips() {
         )
     }
 
+
+    // ========= Afegit al Nivell 1 Exercici 3 =========
+    // Control del numero de pagina
+    const [PageNum, setPageNum] = useState(2)
+    // Pagina amunt i avall
+    const LastPage = () => setPageNum(PageNum - 1)
+    const NextPage = () => setPageNum(PageNum + 1)
+
+    // =vvv Canvi Tots x Pag al N01Ex03 vvv=
+    // Obtenim la Pagina de Naus i l' assignem a l' estat (componentDidMount)
+    useEffect(() => {
+        (async () => {
+            const data = await GetPage(PageNum)
+            setPaginaNaus(data)
+        })
+            ()
+    }, [PageNum]);
+
     // === Render del nom i model de les naus
     return (
         <div className="App">
@@ -67,8 +80,13 @@ export function StarShips() {
                     <h2>Star Wars (N01 Ex02) </h2>
                 </div>
                 <ol>
-                    <PrintTotesNaus />
+                    <PrintPaginaNaus />
                 </ol>
+                <tr>
+                    <td><button onClick={LastPage}>{'<< Last Page'}</button></td>
+                    <td>Page Num: {PageNum}</td>
+                    <td><button onClick={NextPage}>{'Next Page >>'}</button></td>
+                </tr>
                 {<h6>(Clik on StartShip to see details)</h6>}
                 <div>
                     <h3>StarShip Datasheet</h3>
@@ -76,7 +94,8 @@ export function StarShips() {
                 </div>
             </header>
         </div>
-    );
+    )
 }
+// =^^^ Canvi Tots x Pag al N01Ex03 ^^^=
 
 // export default StarShips
