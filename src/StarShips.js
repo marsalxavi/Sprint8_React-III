b8837d78b0f76aa0c50a3bd7ed6f43af39a7e811
marsalxavi <marsalxavi@gmail.com>
2021-08-31 15:18:24 +0200

    S8N01Ex02

--- src/StarShips.js
+++ src/StarShips.js
@@ -1,7 +1,8 @@
 
 import React, { Component, useEffect, useState } from 'react'
 
-export async function GetData() {
+// Crida a la API
+async function GetData() {
     const shipsFetch = await fetch('https://swapi.dev/api/starships/');
     const shipsJson = await shipsFetch.json();
     const shipsArray = shipsJson.results
@@ -12,8 +13,9 @@ export async function GetData() {
 export function StarShips() {
     // Definim l' estat de les naus
     const [totesNaus, setTotesNaus] = useState([])
+    const [unaNau, setUnaNau] = useState([])
 
-    // Obtenim el llistat de Naus i l' assignem a l' estat
+    // Obtenim el llistat de Naus i l' assignem a l' estat (componentDidMount)
     useEffect(() => {
         (async () => {
             const data = await GetData()
@@ -23,26 +25,58 @@ export function StarShips() {
             ()
     }, []);
 
+    //Definim estil de linia
+    var liniaEsquerra = { textAlign: 'Left' }
+
+    // Totes les naus
     // Retorna nom - model
-    function nomModel(nau) {
-        return [nau.name, nau.model].join(" / ");
+    const NomImodel = (aryUnaNau) => ([aryUnaNau.name, aryUnaNau.model].join(" / "))
+
+    // Obte una nau per l index
+    const GetUnaNau = (aryUnaNau) => setUnaNau(aryUnaNau)
+
+    // Llista totes les naus per pantalla
+    function PrintTotesNaus() {
+        return (
+            totesNaus.map((aryUnaNau, index) =>
+            (<div key={index} style={liniaEsquerra}>
+                <li onClick={() => GetUnaNau(aryUnaNau)} > {NomImodel(aryUnaNau)}</li>
+            </div>)
+            )
+        )
     }
 
-    // Render del nom i model de les naus
+    // Llista una Nau per pantalla
+    function FitxaUnaNau() {
+        return (
+            Object.keys(unaNau).map((obj, idx) => {
+                return (
+                    <div key={idx} style={liniaEsquerra}>
+                        {idx + 1} - {Object.keys(unaNau)[idx]}: {Object.values(unaNau)[idx]}
+                    </div>
+                )
+            })
+        )
+    }
+
+    // === Render del nom i model de les naus
     return (
         <div className="App">
             <header className="App-header">
-                <h2>Star Wars</h2>
+                <div>
+                    <h2>Star Wars (N01 Ex02) </h2>
+                </div>
                 <ol>
-                    <div>
-                        {totesNaus.map(i => (
-                            <li style={{ textAlign: "left" }} >{nomModel(i)}}</li>)
-                        )}
-                    </div>
+                    <PrintTotesNaus />
                 </ol>
+                {<h6>(Clik on StartShip to see details)</h6>}
+                <div>
+                    <h3>StarShip Datasheet</h3>
+                    <ul><FitxaUnaNau /></ul>
+                </div>
             </header>
         </div>
     );
 }
 
-// export default StarShips;
+// export default StarShips
